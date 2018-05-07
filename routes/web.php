cc780c4190b4d06a64ab568893667c6f87651362
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-05-07 22:31:35 +0700

    Update source

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/{pagename?}', function () {
+    // echo '<pre>'; print_r(URL::getRequest()->cookie()); echo '</pre>';
+    // echo '<pre>'; print_r(URL::getRequest()->query()); echo '</pre>';
+    // echo '<pre>'; print_r(URL::getRequest()->getUriForPath('')); echo '</pre>';
+    // echo '<pre>'; print_r(URL::getRequest()->getUri()); echo '</pre>';
+    // echo '<pre>'; print_r(URL::getRequest()->getSchemeAndHttpHost()); echo '</pre>';
+    // echo '<pre>'; print_r(URL::getRequest()->getRequestUri()); echo '</pre>';
+    // echo '<pre>'; print_r(URL::getRequest()->getHttpHost()); echo '</pre>';
+    // echo '<pre>'; print_r(URL::getRequest()->getSession()); echo '</pre>';
+    // echo '<pre>'; print_r(URL::getRequest()->fingerprint()); echo '</pre>';
+    // echo '<pre>'; print_r(get_class_vars(get_class(URL::getRequest()))); echo '</pre>';
+    // echo '<pre>'; print_r(get_class_methods(URL::getRequest())); echo '</pre>';
+    
     return view('welcome');
 });
